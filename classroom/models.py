e4d9a934e84feb8d578b6d7d4fe5a7938a9ee049
Elvandro <elvisamuni22@gmail.com>
2019-01-29 12:07:05 +0300

    Created the user profile model

--- classroom/models.py
+++ classroom/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    ROLE_CHOICES = (
+    ('Student', 'Student'),
+    ('Teacher', 'Teacher'),
+    ('Admin', 'Admin'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=300)
+    role = models.CharField(max_length=200, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return self.user.username
